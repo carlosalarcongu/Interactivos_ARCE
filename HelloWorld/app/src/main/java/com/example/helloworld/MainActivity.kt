fd988de93c64687df5ba0729169d4a5990a808c1
Raul Zamora <raul.zamora@canonical.com>
2026-02-11 19:04:30 +0100

    gps

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,20 +1,66 @@
 package com.example.helloworld
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+class MainActivity : AppCompatActivity(), SensorEventListener, LocationListener {
     private lateinit var sensorManager: SensorManager
     private var accelerometer: Sensor? = null
+
+    private var lat = 0.0
+    private var lon = 0.0
+    private lateinit var locationManager: LocationManager
+
+    override fun onLocationChanged(location: Location) {
+        lat = location.latitude
+        lon = location.longitude
+        val visualizer = findViewById<TextView>(R.id.localizacion)
+        visualizer.text = "Latitud: $lat, Longitud: $lon"
+
+    }
+
+    private fun iniciarGPS() {
+        try {
+            locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 2000, 1f, this )
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
+    private val permisoGPSLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { concedido ->
+            if (concedido) {
+                iniciarGPS()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Permiso de ubicación denegado",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        comprobarPermisoGPS()
 
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
@@ -38,6 +84,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         if (event != null) {
             val visualizador = findViewById<TextView>(R.id.visualizador)
             visualizador.text = getString(R.string.accelerometer_data, event.values[0], event.values[1], event.values[2])
+        }
+    }
+
+    private fun comprobarPermisoGPS() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            iniciarGPS()
+        } else {
+            permisoGPSLauncher.launch( Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
 }
